197f7d36ed064665ee835685a731548de1d71902
Thimachal <thimachal@gmail.com>
2022-12-15 21:19:19 -0300

    value

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ form.addEventListener('submit', function (e) {
 
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
-    alert(`Atividade: ${inputNomeAtividade} - Nota ${inputNotaAtividade}`)
+    alert(`Atividade: ${inputNomeAtividade.value} - Nota ${inputNotaAtividade.value}`)
 
 })
