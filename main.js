1f98b4fa74113d9d78f601beca460acdea6058f2
Thimachal <thimachal@gmail.com>
2022-12-15 21:16:09 -0300

    nota

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ form.addEventListener('submit',function(e){
     e.preventDefault();
 
     const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
     
 
 })
