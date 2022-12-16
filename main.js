9915353275100d5b99da2aa817c7fa023d0da126
Thimachal <thimachal@gmail.com>
2022-12-15 21:15:46 -0300

    nome

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const form = document.getElementById('form-atividade');
 form.addEventListener('submit',function(e){
     e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    
+
 })
