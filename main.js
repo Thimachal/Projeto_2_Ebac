8d0ae3d80238a7361704f01afb4e4e0e14305bde
Thimachal <thimachal@gmail.com>
2022-12-15 21:18:05 -0300

    note

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const form = document.getElementById('form-atividade');
-form.addEventListener('submit',function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
-    
+    alert(`Atividade: ${inputNomeAtividade}`)
 
 })
