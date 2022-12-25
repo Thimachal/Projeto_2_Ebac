ef4fe729b96a8f69960c122313233fcbcd03a4e1
Thimachal <thimachal@gmail.com>
2022-12-25 13:05:15 -0300

    npotes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form-atividade');
 const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji-celebrando" />';
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji-decepcionado" />';
-const atividades =[];
+const atividades = [];
 const notas = [];
 
 
@@ -21,7 +21,8 @@ function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-atividades.push(inputNomeAtividade.value);
+    atividades.push(inputNomeAtividade.value);
+    notas.push(inputNomeAtividade.value);
 
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
